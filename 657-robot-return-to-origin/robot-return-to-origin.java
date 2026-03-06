f4780bb80deca0934c7f1d246b785ee235f58abf
Ishika <ishikahere04@gmail.com>
2026-03-06 06:20:34 +0530

    Time: 5 ms (67.76%) | Memory: 45.8 MB (61.33%) - LeetSync

---
+++ 657-robot-return-to-origin/robot-return-to-origin.java
@@ -0,0 +1,30 @@
+class Solution {
+    public boolean judgeCircle(String moves) {
+        int x = 0;
+        int y = 0;
+
+        for(char move : moves.toCharArray()){
+            if(move == 'U')
+            {
+                y++;
+            }
+
+            if(move == 'D')
+            {
+                y--;
+            }
+
+            if(move == 'R')
+            {
+                x++;
+            }
+
+            if(move == 'L')
+            {
+              x--;
+            }
+        }
+        if(x == 0  && y == 0) return true;
+        return false;
+    }
+}
