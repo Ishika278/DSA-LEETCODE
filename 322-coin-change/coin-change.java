3d55073ad21c14c2f7d029550acd5c5d846e8836
Ishika <ishikahere04@gmail.com>
2026-04-19 07:16:59 +0530

    Time: 34 ms (14.43%) | Memory: 47.5 MB (11.97%) - LeetSync

--- 322-coin-change/coin-change.java
+++ 322-coin-change/coin-change.java
@@ -1,36 +1,35 @@
 class Solution {
-    public static int solve(int[] arr, int sum, int i, int[][]dp)
-    {  
-        //base cases
+    public static int solve(int[] coin, int sum, int i, int[][] dp)
+    {
         if(sum == 0) return 0;
-        if(sum < 0 || i == arr.length) return 99999;
-        
-        if(dp[i][sum] != -1){
-            return dp[i][sum];
+        if(sum < 0 || i == coin.length) return 99999;
+
+        if(dp[i][sum] != -1)
+        {
+        return dp[i][sum];
         }
 
-        int take = 1 + solve(arr, sum - arr[i], i, dp);
-        int nottake = solve(arr,sum, i+1, dp);
+        int take = 1 + solve(coin, sum - coin[i], i, dp);
+        int nottake = solve(coin, sum, i+1, dp);
 
         dp[i][sum] = Math.min(take, nottake);
+
         return dp[i][sum];
-        
+
     }
     public int coinChange(int[] coins, int amount) {
-      int n = coins.length;
-      int[][] dp = new int[n][amount + 1];
-
-      for(int i = 0; i < n; i++)
-      {
+       int n = coins.length;
+       int[][] dp = new int[n][amount + 1];
+       for(int i = 0; i< n; i++)
+       {
         for(int j = 0; j < amount + 1; j++)
         {
-            dp[i][j] = -1;
+          dp[i][j] = -1;
         }
-      }
+       }
+
        int ans = solve(coins, amount, 0, dp);
        if(ans >= 99999) return -1;
-
-       return ans;
-       // return solve(coins, amount, 0, dp);
+       return ans; 
     }
 }
