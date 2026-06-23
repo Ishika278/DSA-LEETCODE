5a3c950ef4c9f49d4e61cad9ede417e817bd916b
Ishika <ishikahere04@gmail.com>
2026-06-23 10:45:18 +0530

    Time: 87 ms (0.43%) | Memory: 47.1 MB (12.42%) - LeetSync

--- 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.java
+++ 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.java
@@ -1,25 +1,50 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
+// /**
+//  * Definition for a binary tree node.
+//  * public class TreeNode {
+//  *     int val;
+//  *     TreeNode left;
+//  *     TreeNode right;
+//  *     TreeNode() {}
+//  *     TreeNode(int val) { this.val = val; }
+//  *     TreeNode(int val, TreeNode left, TreeNode right) {
+//  *         this.val = val;
+//  *         this.left = left;
+//  *         this.right = right;
+//  *     }
+//  * }
+//  */
 class Solution {
     public int maxDepth(TreeNode root) {
-        
-        if(root == null) return 0;
-        int left = maxDepth(root.left);
-        int right = maxDepth(root.right);
-        
-        return Math.max(left, right)+ 1;
+
+        List<List<Integer>> ans = new ArrayList<>();
+        int height = getHeight(root);
+
+        for (int i = 0; i < height; i++) {
+            ans.add(solve(i, root));
+        }
+        return ans.size();
+    }
+
+    public int getHeight(TreeNode root) {
+        if (root == null)
+            return 0;
+
+        return 1 + Math.max(getHeight(root.left), getHeight(root.right));
+    }
+
+    public static List<Integer> solve(int level, TreeNode root) {
+        List<Integer> temp = new ArrayList<>();
+        if (root == null)
+            return temp;
+
+        if (level == 0) {
+            temp.add(root.val);
+        } else {
+            temp.addAll(solve(level - 1, root.left));
+            temp.addAll(solve(level - 1, root.right));
+        }
+
+        return temp;
     }
 }
+
