20583414d05615afa2cbacdb6a7f314bc8e500d9
Ishika <ishikahere04@gmail.com>
2026-06-23 10:39:41 +0530

    Time: 111 ms (1.69%) | Memory: 47.3 MB (7.37%) - LeetSync

--- 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.java
+++ 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.java
@@ -15,28 +15,35 @@
  */
 class Solution {
     public List<List<Integer>> levelOrder(TreeNode root) {
+
         List<List<Integer>> ans = new ArrayList<>();
-        if(root == null) return ans;
+        int height = getHeight(root);
 
-        Queue<TreeNode> q = new LinkedList<>();
-        q.offer(root);
-
-        while(!q.isEmpty())
-        {
-            int size = q.size();
-            List<Integer> level = new ArrayList<>();
-
-            for(int i = 0; i < size; i++)
-            {
-                TreeNode node = q.poll();
-                level.add(node.val);
-
-                if(node.left != null) q.offer(node.left);
-                if(node.right != null) q.offer(node.right);
-            }
-            ans.add(level);
+        for (int i = 0; i < height; i++) {
+            ans.add(solve(i, root));
         }
         return ans;
+    }
 
+    public int getHeight(TreeNode root) {
+        if (root == null)
+            return 0;
+
+        return 1 + Math.max(getHeight(root.left), getHeight(root.right));
+    }
+
+    public static List<Integer> solve(int level, TreeNode root) {
+        List<Integer> temp = new ArrayList<>();
+        if (root == null)
+            return temp;
+
+        if (level == 0) {
+            temp.add(root.val);
+
+        } else if (level > 0) {
+            temp.addAll(solve(level - 1, root.left));
+            temp.addAll(solve(level - 1, root.right));
+        }
+        return temp;
     }
 }
